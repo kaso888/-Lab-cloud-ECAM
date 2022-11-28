5bc04bcb52f5e556970dff98b83d24a31eea5f80
FRABOULET Xavier - m063558 <xavier.fraboulet@soprasteria.com>
2022-11-28 15:48:32 +0100

    Add H2 backend

---
+++ lab/api/init-vm-api-h2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt update
+sudo apt -y install openjdk-11-jdk mariadb-client
+
+sudo mkdir /usr/local/applications
+sudo chmod 777 /usr/local/applications
+cd /usr/local/applications
+
+
+echo "Downloding API app"
+curl https://gitlab.com/ecam/lab1/-/raw/main/lab/api/bplace-h2.jar?inline=false --output bplace.jar
+
+chmod +x launch.sh
+nohup java -jar bplace.jar &
+
+
+echo "API app created"
