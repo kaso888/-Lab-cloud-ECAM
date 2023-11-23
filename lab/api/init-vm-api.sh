79603554a14514dc50e1e6feb3727af490b9e372
Xavier <xavier@ArkBookNext.local>
2023-11-23 17:03:12 +0100

    Update backend to golang

--- lab/api/init-vm-api.sh
+++ lab/api/init-vm-api.sh
@@ -5,11 +5,9 @@ cd /usr/local/applications
 
 
 echo "Downloding API app"
-curl https://gitlab.com/ecam-ssg/lab/-/raw/main/lab/api/lab-back.tar?inline=false --output lab-back.tar
+curl https://gitlab.com/ecam-ssg/lab/-/raw/main/lab/api/lab-back?inline=false --output lab-back
 curl https://gitlab.com/ecam-ssg/lab/-/raw/main/lab/api/launch.sh?inline=false --output launch.sh
 curl https://gitlab.com/ecam-ssg/lab/-/raw/main/lab/api/properties.ini?inline=false --output properties.ini
-
-tar xvf lab-back.tar
 
 chmod +x launch.sh
 nohup ./launch.sh &
