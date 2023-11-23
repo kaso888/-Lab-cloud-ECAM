92d141751c50914aa8ecb00e057e06f5b6243c76
Xavier <xavier@ArkBookNext.local>
2023-11-23 16:59:07 +0100

    Update backend to golang

--- lab/api/init-vm-api.sh
+++ lab/api/init-vm-api.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-sudo apt update
-sudo apt -y install openjdk-11-jdk mariadb-client
-
 sudo mkdir /usr/local/applications
 sudo chmod 777 /usr/local/applications
 cd /usr/local/applications
 
 
 echo "Downloding API app"
-curl https://gitlab.com/ecam-ssg/lab/-/raw/main/lab/api/bplace.jar?inline=false --output bplace.jar
+curl https://gitlab.com/ecam-ssg/lab/-/raw/main/lab/api/lab-back.tar?inline=false --output lab-back.tar
 curl https://gitlab.com/ecam-ssg/lab/-/raw/main/lab/api/launch.sh?inline=false --output launch.sh
-curl https://gitlab.com/ecam-ssg/lab/-/raw/main/lab/api/launch.env?inline=false --output launch.env
+curl https://gitlab.com/ecam-ssg/lab/-/raw/main/lab/api/properties.ini?inline=false --output properties.ini
+
+tar xvf lab-back.tar
 
 chmod +x launch.sh
 nohup ./launch.sh &
-
 
 echo "API app created"
