a60c0e4ccf2a9fb1a310690e569ac3c857fe0ee9
Xavier <xavier@ArkBookNext.local>
2023-11-23 16:33:55 +0100

    Fix init vm web

--- lab/web/init-vm-web.sh
+++ lab/web/init-vm-web.sh
@@ -6,7 +6,7 @@ echo "Downloding React app"
 cd /tmp/
 curl https://gitlab.com/ecam-ssg/lab/-/raw/main/lab/web/lab-web.tar?inline=false --output lab-web.tar
 tar xvf lab-web.tar
-sudo cp build/*  /var/www/html/
+sudo cp -R build/*  /var/www/html/
 
 curl https://gitlab.com/ecam-ssg/lab/-/raw/main/lab/web/nginx.conf?inline=false --output nginx.conf
 sudo cp nginx.conf /etc/nginx/sites-available/default
